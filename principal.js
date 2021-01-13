7607200871052be6341182cb3c71a1f314dcdd24
HugoPoletto34 <hugopoletto34@gmail.com>
2021-01-12 22:15:37 -0300

    Invert order of the functions

--- principal.js
+++ principal.js
@@ -2,26 +2,33 @@ import Deque from './myWorks/Fila/fila-dupla.js'
 import Queue from './myWorks/Fila/fila.js'
 import Stack from './myWorks/Pilha/stack.js'
 
-/* Chamada da classe fila dupla */
-function MainDeque() {
-    const deque = new Deque();
-    console.log(deque.isEmpty());
-    deque.insereElementoNoFim('John')
-    deque.insereElementoNoFim('Jack')
-    console.log(deque.imprime());
-    deque.insereElementoNoFim('Camila')
-    console.log(deque.imprime());
-    console.log(deque.size());
-    console.log(deque.isEmpty());
-    console.log(deque.imprime());    
-    console.log("Removido Frente: " + deque.removeElementoNaFrente());
-    console.log(deque.imprime());
-    console.log("Removido Trás: " + deque.removeElementoNoFim());
-    console.log(deque.imprime());
-    console.log("John retorna para pedir informação: ")
-    deque.insereElementoNaFrente('John');
-    console.log(deque.imprime());
+// Jogo Batata Quente usando a classe fila simples
+function hotPotato(elementsList = names, num) {
+    const queue = new Queue();
+
+    const elimidatedList = [];
+    for(let i = 0; i < elementsList.length; i++) 
+        queue.insereElemento(elementsList[i])
+
+    while(queue.size() > 1) {
+        for(let i = 0; i < num; i++)
+            queue.insereElemento(queue.removeElemento());
+        elimidatedList.push(queue.removeElemento());
+    }
+
+    return {
+        eliminated: elimidatedList,
+        winner: queue.removeElemento()
+    }
 }
+/*
+const names = ['John', 'jack', 'Camila', 'Ingrid', 'Carl'] 
+const result = hotPotato(names, 7);
+result.eliminated.forEach(name => {
+    console.log(`${name} was eliminated`)
+})
+console.log("The winner was: " + result.winner);
+*/
 
 /* Conversor de Base usando classe Pilha */
 function baseConverter (decNumber, base) {
@@ -65,30 +72,23 @@ function decimalToBinary(decNumber) {
 }
 // for(let i = 0; i < 20; i++) console.log(i + ": " + decimalToBinary(i))
 
-// Jogo Batata Quente usando a classe fila simples
-function hotPotato(elementsList = names, num) {
-    const queue = new Queue();
-
-    const elimidatedList = [];
-    for(let i = 0; i < elementsList.length; i++) 
-        queue.insereElemento(elementsList[i])
-
-    while(queue.size() > 1) {
-        for(let i = 0; i < num; i++)
-            queue.insereElemento(queue.removeElemento());
-        elimidatedList.push(queue.removeElemento());
-    }
-
-    return {
-        eliminated: elimidatedList,
-        winner: queue.removeElemento()
-    }
+/* Chamada da classe fila dupla */
+function MainDeque() {
+    const deque = new Deque();
+    console.log(deque.isEmpty());
+    deque.insereElementoNoFim('John')
+    deque.insereElementoNoFim('Jack')
+    console.log(deque.imprime());
+    deque.insereElementoNoFim('Camila')
+    console.log(deque.imprime());
+    console.log(deque.size());
+    console.log(deque.isEmpty());
+    console.log(deque.imprime());    
+    console.log("Removido Frente: " + deque.removeElementoNaFrente());
+    console.log(deque.imprime());
+    console.log("Removido Trás: " + deque.removeElementoNoFim());
+    console.log(deque.imprime());
+    console.log("John retorna para pedir informação: ")
+    deque.insereElementoNaFrente('John');
+    console.log(deque.imprime());
 }
-/*
-const names = ['John', 'jack', 'Camila', 'Ingrid', 'Carl'] 
-const result = hotPotato(names, 7);
-result.eliminated.forEach(name => {
-    console.log(`${name} was eliminated`)
-})
-console.log("The winner was: " + result.winner);
-*/
